3563af79eae27e1ad526adf7fcc4b4666285dc63
johnzzx <johnzzx@umich.edu>
2016-11-16 10:17:50 -0500

    johnzzx's test

--- app/src/main/java/com/example/chnwo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chnwo/myapplication/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.chnwo.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -19,15 +21,21 @@ public class MainActivity extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_main);
 
         //link widget to UI
-        buttongo = (Button) findViewById(R.id.buttongo)
+        buttongo = (Button) findViewById(R.id.buttongo);
         //start listener
         buttongo.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+    //this will happen when the button is clicked
+
+       // Toast.makeText(this, "good job!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainjohnActivity.class);
+        startActivity (intent);
 
     }
+
 }
 
 // this is johnzzx's test for chnwosu's my app 3
--- app/src/main/java/com/example/chnwo/myapplication/MainjohnActivity.java
+++ app/src/main/java/com/example/chnwo/myapplication/MainjohnActivity.java
@@ -1,10 +1,12 @@
 package com.example.chnwo.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainjohnActivity extends AppCompatActivity {
+public class MainjohnActivity extends AppCompatActivity implements View.OnClickListener{
 
     //declare all widgets
     private Button buttongo;
@@ -13,5 +15,21 @@ public class MainjohnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainjohn);
+
+        //link widget to UI
+        buttongo = (Button) findViewById(R.id.buttongo);
+        //start listener
+        buttongo.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        //this will happen when the button is clicked
+
+        // Toast.makeText(this, "good job!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity (intent);
+
     }
 }
